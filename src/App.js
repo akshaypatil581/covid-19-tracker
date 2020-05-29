f20bd4848947f113850139071c9e96120ccb3794
akshaypatil581 <akshaypatil581@gmail.com>
2020-05-28 19:05:11 -0700

    Fetched data from api, removed unnecessary data, added material ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React from 'react';
 
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker/CountryPicker';
+
+// Instead of having many imports it's better to have one import like in following line
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+
 class App extends React.Component {
+    state = {
+        data: {},
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({ data: fetchedData } );
+        
+    }
+
     render() {
+        const { data } = this.state;
+
         return (
-            <div>
-                <h1>App</h1>
+            <div className={styles.container}>
+                <Cards data={data}/>
+                <CountryPicker />
+                <Chart />
             </div>
         )
     }
 }
 
 export default App;
+
+/* 
+    writing this way ensures where we are getting our style from 
+    <div className={styles.container}>
+
+    here we are writing async in front of componentDidMount coz it's 
+    built in way of doing it but generally for other functions we write it as 
+    function_name async() {}
+
+*/
---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+/*
+    most modern way of deling with asychronus data is to use async 
+    it deals with api request the same way as promises does but it's more
+    easier to read and write 
+*/
+export const fetchData = async () => {
+    try {
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+                
+        return { confirmed, recovered, deaths, lastUpdate };  
+    } catch (error) {
+
+    }
+}
+
+/*
+Here we are doing destructuring of data if we don't do the destructuring it will
+show undefined { data } - thats the way of destructuring provided in React
+
+{ data: { confirmed, recovered, deaths, lastUpdate } } - this is the way of 
+further destructuring the data
+this line takes on confirmed, recovered, deaths, lastUpdate values from data object
+
+*/
